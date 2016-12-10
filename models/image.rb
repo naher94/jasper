992889aa9d0b699c158e6654f1acb417c66fe9e8
Rehan Butt <rab@Rehans-MacBook-Pro-2.local>
2016-12-10 00:01:00 -0500

    Dribbble Image Scrape Added

--- models/image.rb
+++ models/image.rb
@@ -3,5 +3,36 @@ class Image < ActiveRecord::Base
 	belongs_to :swatch 
 	#has_many :images  
 	#validates_presence_of :url
+	def self.save_image_data(data)
+	  swatch = Swatch.new
+	  
+		for i in data.length
+			#poulate
+			image.date = Time.now
+			image.image = data[i]
+			#save
+			image.save!
+		end
+	end
 
+	def self.get_dribbble_photos(hex)
+	  url = "https://dribbble.com/colors/#{ hex.gsub('#', '') }"
+	  document = Nokogiri::HTML(open(url))
+
+	  images = document.css(".dribbble-link noscript img")
+	  image_hrefs = Array.new
+
+	  images.each do | image |
+	    image_hrefs.push(image.attr('src').gsub('_teaser.','.'))
+	  end
+
+	  return image_hrefs.sample(4)
+	end
+
+	def self.load_image(data) #data is a swatch point
+		#get hex from the lastest entry in the db
+		#pass it in as a n argument
+		hex = data.hex
+		self.save_image_data(self.get_dribbble_photos(hex))
+	end
 end
