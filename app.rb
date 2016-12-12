4fb04c5137cc0a45aef7643c54f31dfc7f37a78e
Rehan Butt <rab@Rehans-MacBook-Pro-2.local>
2016-12-11 23:59:58 -0500

    String formatting

--- app.rb
+++ app.rb
@@ -157,11 +157,11 @@ end
 
 def color_of_the_day #colorize
   
-  #JSON.load(s.themetic_words)
+  words = JSON.load(colorToday.themetic_words)
   colorToday = Swatch.where( "DATE(date) = ?", Date.today).first
   #show an image of the color swatch
 
-  "Today's color is #{colorToday.name} #{colorToday.pantone} #{colorToday.hex}, which stands for #{colorToday.themetic_words[0]}"
+  "Today's color is #{colorToday.name} #{colorToday.pantone} #{colorToday.hex}, which stands for #{words[0].downcase}"
 
   # "Pantone's color of the day is Canton (16-5112) #6CA3A1, which stands for \'Powerful, Dynamic & Introspective\' " + "swatchPlaceHolder.png" + "Want images using canton?"
 end
