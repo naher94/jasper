acb6d8089ee1898f6cd2315adb9d92a1e6a9005b
Rehan Butt <rab@Rehans-MacBook-Pro-2.local>
2016-12-11 23:50:43 -0500

    Downcase not effective

--- app.rb
+++ app.rb
@@ -57,7 +57,7 @@ get '/incoming_sms' do
   image_url = nil
   images = []
   
-  if GREETINGS.downcase.include? body
+  if GREETINGS.include? body
     message = get_greeting
     image_url = "http://rehanbutt.com/img/colorBot/jasper.png"
   end
@@ -140,7 +140,7 @@ private
 
 CONFIRMATIONS = ["Yes","Yup","Totally","Totes","👍"]
 
-GREETINGS = ["Hi","Yo", "hey","howdy", "hello", "ahoy", "‘ello", "aloha", "hola", "bonjour", "hallo", "ciao", "konnichiwa"]
+GREETINGS = ["hi","yo", "hey","howdy", "hello", "ahoy", "‘ello", "aloha", "hola", "bonjour", "hallo", "ciao", "konnichiwa"]
 
 COMMANDS = "hi, who, what, where, when, why and play."
 
