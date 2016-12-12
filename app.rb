bb65ede03bfdfa45ce5f0113b9a19b1659cd9244
Rehan Butt <rab@Rehans-MacBook-Pro-2.local>
2016-12-12 01:35:19 -0500

    Syntax

--- app.rb
+++ app.rb
@@ -200,7 +200,8 @@ end
 
 def yesterday_color_text
   colorYesterday = Swatch.where( "DATE(date) = ?", Date.today.prev_day).first
-  "Yesterday's color was #{colorToday.name} #{colorToday.pantone} #{colorToday.hex}, which stands for #{words[0].downcase}, #{words[1].downcase}, #{words[2].downcase}"
+  words = JSON.load(colorYesterday.themetic_words)
+  "Yesterday's color was #{colorYesterday.name} #{colorYesterday.pantone} #{colorYesterday.hex}, which stands for #{words[0].downcase}, #{words[1].downcase}, #{words[2].downcase}"
 
 end
 
